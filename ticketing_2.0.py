1494d23b23a2481ac8270053544e2c2a0f2abc5e
okey1493 <99826054+okey1493@users.noreply.github.com>
2023-08-13 14:44:56 -0400

    Create ticketing_2.0.py multiple if statements

---
+++ ticketing_2.0.py
@@ -0,0 +1,23 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+if height >= 120:
+  print("you are qualified to ride the rollercoaster")
+  age = int(input("what is your age"))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7")
+  else:
+    bill = 12
+    print("Adult tickets are $12")
+  
+  wants_photo = input("would you like to take a photo? Y or N. ")
+  if wants_photo == "Y":
+     bill += 3
+  print(f"your total bill is ${bill} ")
+  
+else:
+  print("you need to grow some height so come back later '-' ")
