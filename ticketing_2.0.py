a57bd2d411e2e922c0ad12596ae0f9f216024c39
okey1493 <99826054+okey1493@users.noreply.github.com>
2023-08-14 00:12:32 -0400

    ticketing_2.0.py
    A logical function was added  to exempt a different age range from paying for the ticket.

--- ticketing_2.0.py
+++ ticketing_2.0.py
@@ -3,13 +3,15 @@ height = int(input("What is your height in cm? "))
 bill = 0
 if height >= 120:
   print("you are qualified to ride the rollercoaster")
-  age = int(input("what is your age"))
+  age = int(input("what is your age? "))
   if age < 12:
     bill = 5
     print("Child tickets are $5")
   elif age <= 18:
     bill = 7
     print("Youth tickets are $7")
+  elif age >= 45 and age <=55:
+    print("You can have a free ride. This is on the house")
   else:
     bill = 12
     print("Adult tickets are $12")
